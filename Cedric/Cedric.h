5e1c85213c38baa3736752d570bc0af74dabe1e6
Szymon Mrozek <szymon.mrozek@appunite.com>
2018-05-06 19:08:24 +0200

    Initial commit

---
+++ Cedric/Cedric.h
@@ -0,0 +1,19 @@
+//
+//  Cedric.h
+//  Cedric
+//
+//  Created by Szymon Mrozek on 06.05.2018.
+//  Copyright © 2018 AppUnite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cedric.
+FOUNDATION_EXPORT double CedricVersionNumber;
+
+//! Project version string for Cedric.
+FOUNDATION_EXPORT const unsigned char CedricVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cedric/PublicHeader.h>
+
+
