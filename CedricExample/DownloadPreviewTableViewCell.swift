6a65c894fa9b5487579fe71d78ff249b36e6a507
Szymon Mrozek <szymon.mrozek.sm@gmail.com>
2018-05-10 12:45:32 +0200

    Initialized example

---
+++ CedricExample/DownloadPreviewTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  DownloadPreviewTableViewCell.swift
+//  CedricExample
+//
+//  Created by Szymon Mrozek on 10.05.2018.
+//  Copyright © 2018 AppUnite. All rights reserved.
+//
+
+import UIKit
+
+class DownloadPreviewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+
+    }
+
+}
---
+++ CedricExample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CedricExample
+//
+//  Created by Szymon Mrozek on 10.05.2018.
+//  Copyright © 2018 AppUnite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
