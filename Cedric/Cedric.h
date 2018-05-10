84b3ba9c983cb76dcc027404340af7a1be423ca3
Szymon Mrozek <szymon.mrozek.sm@gmail.com>
2018-05-10 20:47:47 +0200

    Fix macos

--- Cedric/Cedric.h
+++ Cedric/Cedric.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018 AppUnite. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Cedric.
 FOUNDATION_EXPORT double CedricVersionNumber;
